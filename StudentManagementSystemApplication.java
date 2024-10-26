793b9119ef71641af23ece54f7684fce598354b8
jatinarora31 <149509412+jatinarora31@users.noreply.github.com>
2024-10-26 22:15:05 +0530

    Add files via upload

---
+++ StudentManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package in.mainflow.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
+
+@SpringBootApplication
+public class StudentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+
+}
